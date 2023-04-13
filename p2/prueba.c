225523778b19a011cacc56cfae3d11b26e95b278
Sergio Castrillón <88632177+sergiocastrillon@users.noreply.github.com>
2023-04-13 19:18:51 +0200

    p2

--- p2/prueba.c
+++ p2/prueba.c
@@ -1,46 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <string.h>
 #include <mpi.h>
-
-void inicializaCadena(char *cadena, int n){
-    int i;
-    for(i=0; i<n/2; i++){
-        cadena[i] = 'A';
-    }
-    for(i=n/2; i<3*n/4; i++){
-        cadena[i] = 'C';
-    }
-    for(i=3*n/4; i<9*n/10; i++){
-        cadena[i] = 'G';
-    }
-    for(i=9*n/10; i<n; i++){
-        cadena[i] = 'T';
-    }
-}
+#include <string.h>
 
 
-int MPI_BinomialBcast(void * buff, int count, MPI_Datatype datatype, 
+void MPI_BinomialBcast(void * buf, int count, MPI_Datatype datatype, 
 int root,MPI_Comm comm){
     int numprocs, rank;
     MPI_Comm_size(comm, &numprocs);
     MPI_Comm_rank(comm, &rank);
-    
+
     for(int i = 1; pow(2,i-1) <= numprocs; i++){
+        int copy = rank;
         int pot = pow(2,i-1);
         // Si tu rango es menor que 2 elevado al paso actual - 1 entonces 
         //envias si no recibes
         if(rank < pot){ 
             if(rank + pot >= numprocs) break; // Para no multiplos de 2 si te pasas del numero de procesos rompe el bucle
-            MPI_Send(&buff,count,datatype,rank+pot,1,comm);
+            //printf("Proceso %d en iteracion %d mandando a %d\n",rank,i,rank+pot);
+            MPI_Send(&copy,count,datatype,rank+pot,1,comm);
             MPI_Send(&i,1,MPI_INT,rank+pot,1,comm);
-        }else{
-            MPI_Recv(buff,count,datatype,MPI_ANY_SOURCE,1,comm,NULL);
+        } // && pot < numprocs
+        else{
+            //printf("Proceso %d en iteracion %d esperando para recibir\n",rank,i);
+            MPI_Recv(buf,count,datatype,MPI_ANY_SOURCE,1,comm,NULL);
             MPI_Recv(&i,1,MPI_INT,MPI_ANY_SOURCE,1,comm,NULL);
         }
     }
 }
+
+
+
+
+void MPI_Bcast_binomial(void *buffer, int count, MPI_Datatype datatype, int root, MPI_Comm comm) {
+    int rank, size;
+    MPI_Comm_rank(comm, &rank);
+    MPI_Comm_size(comm, &size);
+
+    int rounds = (int)(log2(size)) + 1;
+    for (int r = 0; r < rounds; r++) {
+        int distance = (int)pow(2, r);
+        if (rank % distance == 0) {
+            int partner = rank + distance;
+            if (partner < size) {
+                MPI_Send(buffer, count, datatype, partner, 0, comm);
+            }
+        } else {
+            int partner = rank - distance;
+            if (partner < 0) {
+                continue;
+            }
+            MPI_Recv(buffer, count, datatype, partner, 0, comm, MPI_STATUS_IGNORE);
+            break;
+        }
+    }
+}
+
 
 int MPI_FlattreeColectiva(void* buff, void* recvbuff, int count, MPI_Datatype datatype,
 MPI_Op op, int root, MPI_Comm comm){
@@ -77,18 +93,10 @@ MPI_Op op, int root, MPI_Comm comm){
 
 }
 
-
 int main(int argc, char *argv[]){
-    if(argc != 3){
-        printf("Numero incorrecto de parametros\nLa sintaxis debe ser: program n L\n  program es el nombre del ejecutable\n  n es el tamaño de la cadena a generar\n  L es la letra de la que se quiere contar apariciones (A, C, G o T)\n");
-        exit(1); 
-    }
-    
     MPI_Init(&argc, &argv);
 
-    int i, n, count=0;
-    char *cadena;
-    char L;
+    int a;
 
 
     int numprocs, rank, namelen;
@@ -96,33 +104,16 @@ int main(int argc, char *argv[]){
     MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank); 
 
-    if(rank == 0){
-        n = atoi(argv[1]);
-        L = *argv[2];
-    }
-    
-    MPI_Bcast(&n,1,MPI_INT,0,MPI_COMM_WORLD);
-    MPI_Bcast(&L,1,MPI_CHAR,0,MPI_COMM_WORLD);
-    //printf("Proceso %d tiene %d y %c\n",rank,n,L);
-    
-    cadena = (char *) malloc(n*sizeof(char));
-    inicializaCadena(cadena, n);
-
-    for(i=rank; i<n; i+=numprocs){
-        if(cadena[i] == L){
-        count++;
-        }
-    }
+    a = 20;
+    int b;
 
 
-    int count_gen;
-    MPI_FlattreeColectiva(&count,&count_gen,1,MPI_INT,MPI_SUM,0,MPI_COMM_WORLD);
-    
-    free(cadena);
+    MPI_FlattreeColectiva(&a,&b,1,MPI_INT,MPI_SUM,0,MPI_COMM_WORLD);
 
+    if(rank == 0) printf("Hola %d\n",b);
+    //MPI_BinomialBcast(&a,1,MPI_INT,0,MPI_COMM_WORLD);
+    //printf("El proceso %d tiene almacenado el numero %d\n",rank,b);
     MPI_Finalize();
-
-    if(rank == 0) printf("El numero de apariciones de la letra %c es %d\n", L, count_gen);
 
     exit(0);
 }
